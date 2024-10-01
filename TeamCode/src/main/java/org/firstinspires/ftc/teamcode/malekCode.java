c3cc51d055375786f7f360d8f8fe63c1c58982f7
Malek Swilam <Malekgswilam@gmail.com>
2024-10-01 17:40:49 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/malekCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/malekCode.java
@@ -1,3 +1,39 @@
-public class malekCode{
-    //hello
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp (name ="Malek")
+public class malekCode extends OpMode {
+
+    private DcMotor backL;
+    private DcMotor backR;
+    private DcMotor frontL;
+    private DcMotor frontR;
+    private double powerInput;
+
+
+    @Override
+    public void loop(){
+
+    }
+
+    @Override
+    public void init(){
+        backL = hardwareMap.get(DcMotor.class, "motorLeft");
+        backR = hardwareMap.get(DcMotor.class, "motorRight");
+        frontL = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontR = hardwareMap.get(DcMotor.class, "frontRight");
+
+        frontR.setDirection(DcMotor.Direction.REVERSE);
+        backR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
 }
