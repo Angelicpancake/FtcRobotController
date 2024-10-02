512c2e5c5494e3df846d2b555d16ef33a56bc549
GojoJinx <122575296+GojoJinx@users.noreply.github.com>
2024-10-02 18:43:00 -0400

    Adding macnum movement code for teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JinkaMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JinkaMovement.java
@@ -5,85 +5,109 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+
+/*autonomous -> Opmode **this is for autonomous
+ * Linear Opmode -> extends Opmode **this is for teleop */
 @TeleOp(name = "Gaurang")
 
 public class JinkaMovement extends OpMode {
+
+    /*DcMotor is an object type like scanner*/
     private DcMotor backLeft;
     private DcMotor backRight;
     private DcMotor frontLeft;
     private DcMotor frontRight;
-    private double setPower =1;
-
+    /*set power is a variable*/
+    private double setPower = 1;
+    //we need the loop to keep on looking for inputs.
+    // theres nothing here now cus we got nothing lol
     @Override
     public void loop() {
 
     }
 
     @Override
+    //init -> initializes the motors for the hub to read
+    //creates devices in the driver hub
+    // the driver hub is the orange thingy
     public void init() {
         backLeft = hardwareMap.get(DcMotor.class, "motorLeft");
         backRight = hardwareMap.get(DcMotor.class, "motorRight");
         frontLeft = hardwareMap.get(DcMotor.class, "motorLeft");
         frontRight = hardwareMap.get(DcMotor.class, "motorRight");
-
+        //we add the reverse for quality of life
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
     }
-
+    //forward moves the bot forward
     public void forward() {
         backLeft.setPower(setPower);
         backRight.setPower(setPower);
         frontLeft.setPower(setPower);
         frontRight.setPower(setPower);
     }
-
+    //backward moves the bot backwards
     public void backward() {
         backLeft.setPower(-1 * setPower);
         backRight.setPower(-1 * setPower);
         frontLeft.setPower(-1 * setPower);
         frontRight.setPower(-1 * setPower);
     }
-
+    //right moves the bot right
     public void right() {
         backLeft.setPower(-1 * setPower);
         backRight.setPower(setPower);
         frontLeft.setPower(setPower);
         frontRight.setPower(-1 * setPower);
     }
-
+    //left moves the bot left
     public void left() {
         backLeft.setPower(setPower);
         backRight.setPower(-1 * setPower);
         frontLeft.setPower(-1 * setPower);
         frontRight.setPower(setPower);
     }
-
+    //forwardRight moves the bot 45 degrees upright
     public void forwardRight() {
         backLeft.setPower(0);
         backRight.setPower(setPower);
         frontLeft.setPower(setPower);
         frontRight.setPower(0);
     }
-
+    //forwardLeft moves the bot 135 degrees upleft
     public void forwardLeft() {
         backLeft.setPower(setPower);
         backRight.setPower(0);
         frontLeft.setPower(0);
         frontRight.setPower(setPower);
     }
-
+    //backwardRight move the bot 315 degrees down-right
     public void backwardRight() {
         backLeft.setPower(-1 * setPower);
         backRight.setPower(0);
         frontLeft.setPower(0);
         frontRight.setPower(-1 * setPower);
     }
-
+    //backwardLeft move the bot 225 degrees down-left
     public void backwardLeft() {
         backLeft.setPower(0);
         backRight.setPower(-1 * setPower);
         frontLeft.setPower( -1 * setPower);
         frontRight.setPower(0);
+    }
+    //rotateClockwise rotates the bot clockwise ->positive degree change
+    public void rotateClockwise(){
+        backLeft.setPower(setPower);
+        backRight.setPower(-1*setPower);
+        frontLeft.setPower(setPower);
+        frontRight.setPower(-1*setPower);
+    }
+    //rotateCounterclockwise rotates the bot counterclockwise ->negative degree change
+    public void rotateCounterclockwise(){
+        backLeft.setPower(-1*setPower);
+        backRight.setPower(setPower);
+        frontLeft.setPower(-1*setPower);
+        frontRight.setPower(setPower);
     }
 
 }
