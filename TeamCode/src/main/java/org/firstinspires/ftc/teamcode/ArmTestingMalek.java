77864668ba457c5fb2c1c8c6a0fef6f08040c4f5
Malek Swilam <Malekgswilam@gmail.com>
2024-10-13 12:53:59 -0400

    start of arm code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTestingMalek.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp (name = "malekArmCode")
+public class ArmTestingMalek extends OpMode {
+    private DcMotor arm;
+    private double armPosition;
+
+
+
+
+    @Override
+    public void init(){
+        arm = hardwareMap.get(DcMotor.class, "arm");
+
+
+    }
+
+    @Override
+    public void loop() {
+        doub
+    }
+
+
+}
+
+
