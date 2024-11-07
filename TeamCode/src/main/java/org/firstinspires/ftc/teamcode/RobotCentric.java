59e0b95bb6871e356090966419c8abfcb9a1cc35
Malek Swilam <Malekgswilam@gmail.com>
2024-11-07 18:50:49 -0500

    changed class name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCentric.java
@@ -1,16 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp (name ="Malek")
-public class malekCode extends OpMode {//linearop - autonomous/ teleop -> opmode
+public class RobotCentric extends OpMode {//linearop - autonomous/ teleop -> opmode
 
     private DcMotor backL;
     private DcMotor backR;
