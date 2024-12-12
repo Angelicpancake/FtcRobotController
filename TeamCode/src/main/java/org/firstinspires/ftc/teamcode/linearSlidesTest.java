b7ac9fd4b42fe343cefcdbbe87b5eb4b96f5454b
GojoJinx <gaurangjinkaprofessional@gmail.com>
2024-12-12 18:17:04 -0500

    made updated linear Slide code.
    created a backup auto for 4 wheel drive and made new methods for arm, claw, slide, and slide claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearSlidesTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearSlidesTest.java
@@ -4,26 +4,19 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name="Joshua Linear Slide")
+@TeleOp(name="WIP2 Linear Slide")
 public class linearSlidesTest extends OpMode {
-//op mode -> real time control, linearOp -> preset commands (auto)
+    //op mode -> real time control, linearOp -> preset commands (auto)
     private DcMotor linearSlide1;
-    private DcMotor linearSlide2;
     public ElapsedTime runtime;
 
     @Override
     public void init(){
         linearSlide1 = hardwareMap.get(DcMotor.class, "slide1");
-        linearSlide2 = hardwareMap.get(DcMotor.class, "slide2");
-
         linearSlide1.setDirection(DcMotor.Direction.REVERSE); //how it should determine positive/negative positions
-        linearSlide2.setDirection(DcMotor.Direction.REVERSE);
-
         runtime = new ElapsedTime();
         runtime.reset(); //set the counter for runtime to be 0
-
     }
 
     @Override
@@ -35,25 +28,27 @@ public class linearSlidesTest extends OpMode {
         telemetry.update();
         //print out the data of the linear slide position on the game hub
 
+        //have to set the power depending on the tests
 
-        double power = Math.abs(gamepad2.left_stick_y);
+        double power = 0.5;
+
 
         // remember it is reversed -> init
-        if (gamepad2.left_stick_y < -0.1 && linearSlide1.getCurrentPosition()< 3000) {
 
-            linearSlide1.setPower(power);
-            linearSlide2.setPower(power);
+
+        //using a variable for true or false for a and b
+
+
+
+        //up
+        if(gamepad2.right_trigger>0 && linearSlide1.getCurrentPosition()<3000){
+            linearSlide1.setPower(gamepad2.right_trigger*power);
         }
-
-        else if (gamepad2.left_stick_y > 0.1 && linearSlide1.getCurrentPosition() > 3) {
-
-            linearSlide1.setPower(-power);
-            linearSlide2.setPower(-power);
+        else if(gamepad2.left_trigger>0.3){
+            linearSlide1.setPower(gamepad2.left_trigger*-1*power);
         }
         else {
-            linearSlide1.setPower(0.05); //try 0
-            linearSlide2.setPower(0.05);
+            linearSlide1.setPower(0.05);
         }
-
     }
 }
